01b6b9110e8ecd7b554758bfea6271d431e3f0c7
emilbak5 <emilbak5@gmail.com>
2022-05-02 13:34:49 +0200

    s

--- src/main.py
+++ src/main.py
@@ -24,14 +24,14 @@ from bokeh.models import Div, WheelZoomTool, Slider
 from bokeh.models.widgets import Panel, Tabs
 from bokeh.driving import count
 
-from lib.visualization import plotting
-
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 
 print("Project in VSLAM")
 
 
-num_images = 50
+num_images = 4000
 dataset = get_dataset(num_images)
 gt_poses = dataset.poses
 infotest=np.array([1,2,3])
@@ -63,9 +63,9 @@ estimated_path = []
 gt_path.append((gt_poses[0][0, 3], gt_poses[0][2, 3]))
 estimated_path.append((gt_poses[0][0, 3], gt_poses[0][2, 3]))
 
-Visu=plotting.VisualDataSource(gt_path, estimated_path)
-Visu.visualize_paths(gt_path, estimated_path, "Stereo Visual Odometry",
-                file_out=os.path.basename("data") + 'll'".html")
+# Visu=plotting.VisualDataSource(gt_path, estimated_path)
+# Visu.visualize_paths(gt_path, estimated_path, "Stereo Visual Odometry",
+#                 file_out=os.path.basename("data") + 'll'".html")
 
 
 
@@ -99,103 +99,187 @@ graph_size = 1
 
 #     print (graph_size)
 
-    
 
 
 
-current_img_idx = 1
-graph_size = 1
-end_of_program = False
 
-#@count()
-def callback():
-    global current_img_idx
-    global graph_size
-    global num_images
-    global prev_idx
-    global end_of_program
-    #for i in range(len(Visu.gt_path)-1):
-    # gt_path = np.array(Visu.gt_path[t+1])
-    # pred_path = np.array(Visu.pred_path[t+1])
-# while current_img_idx <= num_images - 2:        
-    if current_img_idx < num_images - 2:
-        keyframe_idx = get_next_keyframe(dataset, current_img_idx, graph, orb, graph_size)
+fig, ax = plt.subplots()
+gt_data_x, gt_data_y = [], []
+esti_data_x, esti_data_y = [], []
+error_x, error_y = [], []
 
 
-        kp, desc = get_descripters(keyframe_idx, dataset, orb)
-        add_to_lsh_table(desc, flann)
-        transform, _ = VO.get_pose(kp, desc, dataset, graph, current_img_idx, prev_idx)
-        add_to_graph(transform, desc, kp, graph_size, keyframe_idx, graph)
-        graph_size += 1
+gt_line, = plt.plot([], [], marker='o', color='b')
+esti_line, = plt.plot([], [], marker='o', color='r')
+lines = [gt_line, esti_line]
 
 
-        if graph_size % 7 == 0:
-            pass
-            #bundle adjustment
+i = 0
+prev_max_x = 0
+prev_min_x = 0
+prev_max_y = 0
+prev_min_y = 0
 
-        # visualize_path(graph)
-        idx, loop_closure_found_bool = find_most_similar_image(graph_size, graph, flann) # Is not done
-        if loop_closure_found_bool:
-            pass
-        #     perform_loop_closure(idx, graph)
-        #     update_visualize_path(graph)
-        prev_idx = current_img_idx
-        current_img_idx = keyframe_idx
+def init():
+    for line in lines:
+        line.set_data([],[])
+    return lines
 
-        #print (graph_size)
-        print(current_img_idx)
-
-    else:
-        if end_of_program == False:
-            print("End of program")
-        end_of_program = True
-
-
-    # gt_x, gt_y = gt_path.T
-    #pred_x, pred_y = transform.T
- 
-    pred_path = np.array([transform[0, 3], transform[2, 3]])
-    pred_path_x, pred_path_y = pred_path[0], pred_path[1]
+i = 0
+def update(_):
+    global i
+    global prev_max_x
+    global prev_max_y
+    global prev_min_x
+    global prev_min_y
+    global gt_poses
 
 
-    gt_path = gt_poses[current_img_idx]
-    gt_path = np.array([gt_path[0, 3], gt_path[2, 3]])
-
-
+    gt_path = gt_poses[i][0, 3], gt_poses[i][2, 3]
     gt_path_x, gt_path_y = [gt_path[0], gt_path[1]]
 
+    # esti_path_x = gt_data_x + 10
+    # esti_path_y = gt_data_y + 10
+
+    gt_data_x.append(gt_path_x)
+    gt_data_y.append(gt_path_y)
+    esti_data_x.append(gt_path_x + 10)
+    esti_data_y.append(gt_path_y + 10)
+
+
+    gt_line.set_data(gt_data_x, gt_data_y)
+    esti_line.set_data(esti_data_x, esti_data_y)
+    # if len(xdata) % 10 == 0:
+    #     xdata[i-5] = 4
+    #     ydata[i-5] = -0.5
     
-    pred_path=pred_path.reshape(-1,2)
-    gt_path=gt_path.reshape(-1,2)
+
+    border = 10
+    if gt_path_x + border > prev_max_x:
+        ax.set_xlim(prev_min_x, gt_path_x + border)
+        prev_max_x = gt_path_x + border
+
+    if gt_path_x - border < prev_min_x:
+        ax.set_xlim(gt_path_x - border, prev_max_x)
+        prev_min_x = gt_path_x - border
+
+    if gt_path_y + border > prev_max_y:
+        ax.set_ylim(prev_min_y, gt_path_y + border)
+        prev_max_y = gt_path_y + border
+
+    if gt_path_y - border < prev_min_y:
+        ax.set_ylim(gt_path_y - border, prev_max_y)
+        prev_min_y = gt_path_y - border
+
+    i += 20
+
+    test = 0
+    for i in range(10):
+        test += 10
+    
+    return lines,
+
+ani = FuncAnimation(fig, update, frames=None, interval=10,
+                    init_func=init, blit=False)
+plt.show()
+
+    
+
+
+
+# current_img_idx = 1
+# graph_size = 1
+# end_of_program = False
+
+# #@count()
+# def callback():
+#     global current_img_idx
+#     global graph_size
+#     global num_images
+#     global prev_idx
+#     global end_of_program
+#     #for i in range(len(Visu.gt_path)-1):
+#     # gt_path = np.array(Visu.gt_path[t+1])
+#     # pred_path = np.array(Visu.pred_path[t+1])
+# # while current_img_idx <= num_images - 2:        
+#     if current_img_idx < num_images - 2:
+#         keyframe_idx = get_next_keyframe(dataset, current_img_idx, graph, orb, graph_size)
+
+
+#         kp, desc = get_descripters(keyframe_idx, dataset, orb)
+#         add_to_lsh_table(desc, flann)
+#         transform, _ = VO.get_pose(kp, desc, dataset, graph, current_img_idx, prev_idx)
+#         add_to_graph(transform, desc, kp, graph_size, keyframe_idx, graph)
+#         graph_size += 1
+
+
+#         if graph_size % 7 == 0:
+#             pass
+#             #bundle adjustment
+
+#         # visualize_path(graph)
+#         idx, loop_closure_found_bool = find_most_similar_image(graph_size, graph, flann) # Is not done
+#         if loop_closure_found_bool:
+#             pass
+#         #     perform_loop_closure(idx, graph)
+#         #     update_visualize_path(graph)
+#         prev_idx = current_img_idx
+#         current_img_idx = keyframe_idx
+
+#         #print (graph_size)
+#         print(current_img_idx)
+
+#     else:
+#         if end_of_program == False:
+#             print("End of program")
+#         end_of_program = True
+
+
+#     # gt_x, gt_y = gt_path.T
+#     #pred_x, pred_y = transform.T
+ 
+#     pred_path = np.array([transform[0, 3], transform[2, 3]])
+#     pred_path_x, pred_path_y = pred_path[0], pred_path[1]
+
+
+#     gt_path = gt_poses[keyframe_idx]
+#     gt_path = np.array([gt_path[0, 3], gt_path[2, 3]])
+
+
+#     gt_path_x, gt_path_y = [gt_path[0], gt_path[1]]
+
+    
+#     pred_path=pred_path.reshape(-1,2)
+#     gt_path=gt_path.reshape(-1,2)
     
     
-    temp1=np.array([gt_path_x, pred_path_x]).T
-    xs = list(temp1.reshape(-1,2))
+#     temp1=np.array([gt_path_x, pred_path_x]).T
+#     xs = list(temp1.reshape(-1,2))
 
-    temp2=np.array([gt_path_y, pred_path_y]).T
-    ys = list(temp2.reshape(-1,2))
+#     temp2=np.array([gt_path_y, pred_path_y]).T
+#     ys = list(temp2.reshape(-1,2))
 
-    # print("test")
+#     # print("test")
 
-    diff = np.linalg.norm(gt_path - pred_path, axis=1)
-    print(len(diff))
+#     diff = np.linalg.norm(gt_path - pred_path, axis=1)
+#     print(len(diff))
     
-    colorArray=['blue']
+#     colorArray=['blue']
 
-    data=dict(gtx=gt_path[:, 0], gty=gt_path[:, 1],
-                                        px=pred_path[:, 0], py=pred_path[:, 1],
-                                        diffx=np.arange(len(diff)), diffy=diff,
-                                        disx=xs, disy=ys, color=colorArray)
+#     data=dict(gtx=gt_path[:, 0], gty=gt_path[:, 1],
+#                                         px=pred_path[:, 0], py=pred_path[:, 1],
+#                                         diffx=np.arange(len(diff)), diffy=diff,
+#                                         disx=xs, disy=ys, color=colorArray)
 
-    #print(data)
-    Visu.source.stream(data,100)
+#     #print(data)
+#     Visu.source.stream(data,100)
 
-curdoc().theme='dark_minimal'
+# curdoc().theme='dark_minimal'
 
-curdoc().add_root(Visu.plot)
-curdoc().title = "OHLC"
-curdoc().add_periodic_callback(callback,5000)
-print("here")
+# curdoc().add_root(Visu.plot)
+# curdoc().title = "OHLC"
+# curdoc().add_periodic_callback(callback,5000)
+# print("here")
 
 
 
--- src/test.py
+++ src/test.py
@@ -6,25 +6,52 @@ fig, ax = plt.subplots()
 xdata, ydata = [], []
 ln, = plt.plot([], [], marker='o')
 i = 0
-
+prev_max_x = 0
+prev_min_x = 0
+prev_max_y = 0
+prev_min_y = 0
 
 def init():
-    ax.set_xlim(0, 2*np.pi)
-    ax.set_ylim(-1, 1)
+    # ax.set_xlim(0, 2*np.pi)
+    # ax.set_ylim(-1, 1)
     return ln,
+
+
 
 def update(frame):
     global i
+    global prev_max_x
+    global prev_max_y
+    global prev_min_x
+    global prev_min_y
     xdata.append(frame)
     ydata.append(np.sin(frame))
     ln.set_data(xdata, ydata)
     if len(xdata) % 10 == 0:
         xdata[i-5] = 4
         ydata[i-5] = -0.5
+    
 
-    i += 1   
+    if frame + 0.5 > prev_max_x:
+        ax.set_xlim(prev_min_x, frame + 0.5)
+        prev_max_x = frame + 0.5
+
+    if frame - 0.5 < prev_min_x:
+        ax.set_xlim(frame - 0.5, prev_max_x)
+        prev_min_x = frame - 0.5
+
+    if np.sin(frame) + 0.5 > prev_max_y:
+        ax.set_ylim(prev_min_y, np.sin(frame) + 0.5)
+        prev_max_y = np.sin(frame) + 0.5
+
+    if np.sin(frame) - 0.5 < prev_min_y:
+        ax.set_ylim(np.sin(frame) - 0.5, prev_max_y)
+        prev_min_y = np.sin(frame) - 0.5
+
+    i += 1
+    
     return ln,
 
 ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
-                    init_func=init, blit=True)
+                    init_func=init, blit=False)
 plt.show()
